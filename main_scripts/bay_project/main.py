924665689da9f7b204c6c188c8bb4a3308d91027
Ricardo Valencia Albornoz <r.valencia.az@gmail.com>
2025-03-31 10:52:07 +0100

    Update main.py

--- main_scripts/bay_project/main.py
+++ main_scripts/bay_project/main.py
@@ -64,7 +64,7 @@ def main():
     X_train, X_test, y_train, y_test = train_test_split(
         X, y_encoded, test_size=0.2, random_state=42, stratify=y_encoded
     )
-
+    # stratify is important in the case of imbalance datasets
     scaler = StandardScaler()
     X_train = scaler.fit_transform(X_train)
     X_test  = scaler.transform(X_test)
