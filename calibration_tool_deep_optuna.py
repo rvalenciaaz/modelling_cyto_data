d25b1acc32427f7fbf62aa02d38d15d45b552e56
Ricardo Valencia Albornoz <r.valencia.az@gmail.com>
2025-04-20 16:31:51 +0100

    Update calibration_tool_deep_optuna.py

--- calibration_tool_deep_optuna.py
+++ calibration_tool_deep_optuna.py
@@ -90,6 +90,8 @@ def rebuild_model(best_params, state_dict, features_to_keep, label_encoder):
     # Set correct input/output dimensions
     n_features = len(features_to_keep)
     n_classes = len(label_encoder.classes_)
+    # adding here
+    model.classes_=label_encoder.transform(label_encoder.classes_)
     model.input_dim_ = n_features
     model.output_dim_ = n_classes
 
